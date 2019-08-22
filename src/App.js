e818290bb5c21435af0737518b2ab21fd9e1087f
wizard <vagin31337@gmail.com>
2019-08-22 23:08:21 +0500

    redux

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { Fragment, useEffect } from 'react';
+import React, {Fragment, useEffect} from 'react';
 import SearchBar from "./components/layouts/SearchBar";
 import Logs from "./components/logs/Logs";
 import AddBtn from "./components/layouts/AddBtn";
@@ -6,28 +6,32 @@ import AddLogModal from "./components/logs/AddLogModal";
 import EditLogModal from "./components/logs/EditLogModal";
 import AddTechModal from "./components/techs/AddTechModal";
 import TechListModal from "./components/techs/TechListModal";
+import {Provider} from 'react-redux';
+import store from "./store";
 
 import './App.css';
 import 'materialize-css/dist/css/materialize.min.css';
 import M from 'materialize-css/dist/js/materialize.min';
 
 function App() {
-  useEffect(() => {
-    M.AutoInit();
-  });
-  return (
-    <Fragment>
-      <SearchBar />
-      <div className="container">
-          <AddBtn/>
-          <AddLogModal/>
-          <EditLogModal/>
-          <AddTechModal/>
-          <TechListModal/>
-        <Logs />
-      </div>
-    </Fragment>
-  );
+    useEffect(() => {
+        M.AutoInit();
+    });
+    return (
+        <Provider store={store}>
+            <Fragment>
+                <SearchBar/>
+                <div className="container">
+                    <AddBtn/>
+                    <AddLogModal/>
+                    <EditLogModal/>
+                    <AddTechModal/>
+                    <TechListModal/>
+                    <Logs/>
+                </div>
+            </Fragment>
+        </Provider>
+    );
 }
 
 export default App;

117e26de15c2e1eb5cba30c080f028fe07f53b42
wizard <vagin31337@gmail.com>
2019-08-23 22:58:51 +0500

    redux

--- src/actions/logActions.js
+++ src/actions/logActions.js
@@ -1,4 +1,4 @@
-import {GET_LOGS, SET_LOADING, LOGS_ERROR} from "./types";
+import {GET_LOGS, SET_LOADING, LOGS_ERROR, ADD_LOG, DELETE_LOG} from "./types";
 
 // export const getLogs = () => {
 //     return async (dispatch) => {
@@ -24,6 +24,53 @@ export const getLogs = () => async dispatch => {
         dispatch({
             type: GET_LOGS,
             payload: data
+        });
+    } catch (err) {
+        dispatch({
+            type: LOGS_ERROR,
+            payload: err.response.data
+        });
+    }
+
+};
+
+export const addLog = (log) => async dispatch => {
+    try {
+        setLoading();
+
+        const res = await fetch('/logs', {
+            method: 'POST',
+            body: JSON.stringify(log),
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        });
+        const data = await res.json();
+
+        dispatch({
+            type: ADD_LOG,
+            payload: data
+        });
+    } catch (err) {
+        dispatch({
+            type: LOGS_ERROR,
+            payload: err.response.data
+        });
+    }
+
+};
+
+export const deleteLog = (id) => async dispatch => {
+    try {
+        setLoading();
+
+        await fetch(`/logs/${id}`, {
+            method: 'DELETE',
+        });
+
+        dispatch({
+            type: DELETE_LOG,
+            payload: id
         });
     } catch (err) {
         dispatch({
